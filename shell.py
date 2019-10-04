b8038b41acdd95bfd8b971cff54ac5b8f6fdee49
Daniel Kovacs <dkovacs@deasys.net>
2019-10-04 20:10:47 +1000

    Added basic package structure

---
+++ shell.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+# author: Daniel Kovacs <mondomhogynincsen@gmail.com>
+# licence: MIT <https://opensource.org/licenses/MIT>
+# file: shell.py
+# purpose: interactive demo
+# version: 1.0
+
+
+# ---------------------------------------------------------------------------------------
+# imports
+# ---------------------------------------------------------------------------------------
+
+import sys, os
+from pprint import pprint
+pp = pprint
+
+
+# ---------------------------------------------------------------------------------------
+# package specific imports
+# ---------------------------------------------------------------------------------------
+
+from qtils import *
+
+
+# ---------------------------------------------------------------------------------------
+# main
+# ---------------------------------------------------------------------------------------
+
+# setup your shell here
+# use print() to print help to users
---
+++ src/qtils/__init__.py
@@ -0,0 +1,38 @@
+# encoding: utf-8
+# package: qtils
+# author: Daniel Kovacs <mondomhogynincsen@gmail.com>
+# file-version: 1.0
+
+# -------------------------------------------------------------------------------
+# licence: LGPL-v3
+# This file is part of Ulra Light Weight Qtils.
+#
+# Ulra Light Weight Qtils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Ulra Light Weight Qtils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Ulra Light Weight Qtils. If not, see <https://www.gnu.org/licenses/lgpl>.
+#
+# -------------------------------------------------------------------------------
+
+
+__version__ = "0.8.0"
+
+# -------------------------------------------------------------------------------
+# imports
+# -------------------------------------------------------------------------------
+
+from .collections import *
+
+
+# -------------------------------------------------------------------------------
+# exports
+# -------------------------------------------------------------------------------
+
