b88f0203d72876cb9225f1d471e95594695cf590
Daniel Kovacs <dkovacs@deasys.net>
2019-10-04 20:10:54 +1000

    Added collections

---
+++ src/qtils/collections.py
@@ -0,0 +1,231 @@
+# encoding: utf-8
+# package: qtils
+# author: Daniel Kovacs <github.com/neonihil>
+# file-version: 1.0
+# licence: LGPL-v3
+
+# -------------------------------------------------------------------------------
+# Licence
+# -------------------------------------------------------------------------------
+# This file is part of Ulra Light Weight Qtils.
+#
+# Ulra Light Weight Qtils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Ulra Light Weight Qtils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Ulra Light Weight Qtils. If not, see <https://www.gnu.org/licenses/lgpl>.
+# -------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------
+# imports
+# -------------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# qlist
+# -----------------------------------------------------------------------------
+
+class qlist(list):
+    """
+    Basic ``list`` with a couple shorthands.
+    
+    """
+
+    def get( self, index, default = None ):
+        """Return an ``index``-th element from the list
+        or return ``default`` if not found.
+
+        :param index: Index of the element to return
+        :param default: Value to return if ``index`` not found.
+
+        >>> l = qlist(['foo', 'bar'])
+        >>> l.get(0)
+        'foo'
+        >>> l.get(3, "not found")
+        'not found'
+        """
+        if (index < 0) or ( index >= len(self) ):
+            return default
+        return self[index]
+        
+
+    def register( self, item ):
+        """Add ``item.__name__`` to the list and return 
+        ``item``. This function is meant to be used for 
+        dynamically composing the ``__all__`` list for a python 
+        mudule.
+
+        >>> __all__ = qlist()
+        >>> @__all__.register
+        ... def foo(): pass
+        >>> @__all__.register
+        ... class Bar(object): pass
+        >>> __all__
+        ['foo', 'Bar']
+
+        """
+        self.append( item.__name__ )
+        return item
+
+
+    def __str__(self):
+        """TODO: Document or remove me
+        """
+        return '[' + ', '.join([str(i) for i in self]) + ']'
+
+
+
+# -----------------------------------------------------------------------------
+# exports
+# -----------------------------------------------------------------------------
+
+__all__ = qlist()
+__all__.register(qlist)
+
+
+# -----------------------------------------------------------------------------
+# qdict
+# -----------------------------------------------------------------------------
+
+@__all__.register
+class qdict(dict):
+    """
+    Simple attribute dictionary with a couple shorthands.
+            
+    >>> d = qdict( foo='hello', bar='world' )
+    >>> d
+    {'foo': 'hello', 'bar': 'world'}
+    >>> d.foo
+    'hello'
+    >>> d.answer = 42
+    >>> d
+    {'foo': 'hello', 'bar': 'world', 'answer': 42}
+
+    """
+
+    # def __init__(self, *args, **kw):
+    #     super(qdict,self).__init__( *args, **kw )
+
+
+    def __getattr__(self, key):
+        if not key in self:
+            raise AttributeError(key)
+        return self[key]
+
+
+    def __setattr__(self, key, value):
+        if key.startswith('_') or key in self.__dict__ or key in self.__class__.__dict__:
+            return super(qdict, self).__setattr__(key, value)
+        self[key] = value
+
+
+    def copy( self, add = None ):
+        """Returns a shallow copy of self
+        """
+        res = qdict()
+        res.update( self, False )
+        if add:
+            res.update( add )
+        return res
+
+        
+    def __add__( self, other ):
+        """Supports the `+` operand for two dictionaries
+
+        >>> d1 = qdict(a=1,b=2)
+        >>> d2 = qdict(c=3,b=4)
+        >>> d1 + d2
+        {'a': 1, 'b': 4, 'c': 3}
+
+        """
+        res = self.copy()
+        res.update( other )
+        return res
+
+
+    def update( self, source, recursive = False, add_keys = True, convert = False ):
+        """Recursively updates ``self`` from ``source``.
+    
+        :param source: Source to copy values from to ``self``
+        :type source: ``dict``
+        :param recursive: Recursively update ``dict()`` values, defaults to True
+        :type recursive: bool
+        :param add_keys: Add keys that are in `source` but not in `self`, default to True
+        :type add_keys: bool
+        :param convert: Convert encountered ``dict()``s to ``qdict()``s, defaults to False
+        :type convert: bool
+        :return: ``self``
+
+        Example usage:
+
+        >>> my_dict = qdict(a=1, b=qdict(c=10, d=20))
+        >>> my_dict.update(dict(b=dict(c=5, e=100), f=200))  # calls ``dict.update`` without change
+        {'a': 1, 'b': {'c': 5, 'e': 100}, 'f': 200}
+
+        >>> my_dict = qdict(a=1, b=qdict(c=10, d=20))
+        >>> my_dict.update(dict(b=dict(c=5, e=100), f=200), recursive=True)
+        {'a': 1, 'b': {'c': 5, 'd': 20, 'e': 100}, 'f': 200}
+
+        >>> my_dict = qdict(a=1, b=qdict(c=10, d=20))
+        >>> my_dict.update(dict(b=dict(c=5, e=100), f=200), recursive=True, add_keys=False)
+        {'a': 1, 'b': {'c': 5, 'd': 20}}
+
+        """
+        if not isinstance(source, dict): return self
+        if not recursive:
+            if add_keys:
+                super(qdict,self).update(source)
+                return self
+            for k in self:
+                self[k] = source.get(k,self[k])
+            return self
+        if add_keys:
+            for k, nv in source.items():
+                if convert and isinstance(nv, dict) and (not isinstance(nv, qdict)):
+                    nv_ = qdict()
+                    nv_.update(nv, recursive=recursive, add_keys=add_keys, convert=convert)
+                    nv = nv_
+                if isinstance(nv, dict) and (k in self):
+                    cv = self[k]
+                    if isinstance(cv, dict) and convert:
+                        cv = qdict(cv)
+                        self[k] = cv
+                    if isinstance(cv, qdict):
+                        cv.update(nv, recursive=recursive, add_keys=add_keys, convert=convert)
+                        continue
+                    if isinstance(cv, dict):
+                        cv.update(nv)
+                        continue
+                if convert and isinstance(nv, list):
+                    for i in range(len(nv)):
+                        if isinstance(nv[i], qdict): continue
+                        if isinstance(nv[i], dict):
+                            nnv = qdict()
+                            nnv.update(nv[i], recursive=recursive, add_keys=add_keys, convert=convert)
+                            nv[i] = nnv
+                self[k] = nv
+            return self
+        for k, cv in self.items():
+            try:
+                nv = source[k]
+            except KeyError:
+                continue
+            if convert and isinstance(nv, dict) and (not isinstance(nv, qdict)):
+                nv = qdict(nv)
+            if isinstance(cv, qdict):
+                cv.update(nv, recursive=recursive, add_keys=add_keys, convert=convert)
+            elif isinstance(cv, dict):
+                cv.update(nv)
+            else:
+                self[k] = nv
+        return self
+
+
